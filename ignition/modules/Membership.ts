16378a8d835edfb2831a7875cf905442b7a6aa9e
samclassix <samclassix@proton.me>
2024-11-29 09:49:59 +0100

    feat: hardhat env access in ignition module

--- ignition/modules/Membership.ts
+++ ignition/modules/Membership.ts
@@ -2,7 +2,7 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 import { getChildFromSeed } from '../../helper/wallet';
 import { storeConstructorArgs } from '../../helper/store.args';
 
-const seed = process.env.DEPLOYER_ACCOUNT_SEED;
+const seed = process.env.DEPLOYER_SEED;
 if (!seed) throw new Error('Failed to import the seed string from .env');
 
 const w1 = getChildFromSeed(seed, 1); // admin also deployer
@@ -10,12 +10,12 @@ const w2 = getChildFromSeed(seed, 2); // executor
 const w3 = getChildFromSeed(seed, 3); // member
 
 export const config = {
-	message: 'Config Info: Deploying Module with accounts',
 	admin: w1.address,
 	executor: w2.address,
 	member: w3.address,
 };
 
+console.log('Config Info: Deploying Module with accounts');
 console.log(config);
 
 // constructor args
@@ -26,6 +26,11 @@ console.log('Constructor Args');
 console.log(args);
 
 const MembershipModule = buildModule('MembershipModule', (m) => {
+	// @dev: for runtime specific network tasks
+	// const chainId = hardhat.network.config.chainId;
+	// const params = PARAM[chainId].param01;
+	// const addr = ADDRESS[chainId].contractDeployedAddress;
+
 	// Deploy Membership contract
 	const membership = m.contract('Membership', args);
 
@@ -35,7 +40,7 @@ const MembershipModule = buildModule('MembershipModule', (m) => {
 	// You can add more contracts here
 	// const anotherContract = m.contract('AnotherContract', [...args]);
 
-	// You can add more logic here
+	// You can add more tx logic here
 	// m.call(storage, 'setValue', [100]);
 
 	return {
