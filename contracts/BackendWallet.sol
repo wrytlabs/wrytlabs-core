8a3b16dfbcd412d8ef650c713510d0a73e7376db
samclassix <samclassix@proton.me>
2024-11-30 16:26:05 +0100

    feat: BackendWallet Membership

---
+++ contracts/BackendWallet.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Membership} from './utils/Membership.sol';
+
+contract BackendWallet is Membership {
+	constructor(address admin, address executor, address member) Membership(admin, executor, member) {}
+}
